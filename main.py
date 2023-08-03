7ba42e8669927c127e57fcece4eab52da7ea69cc
Dylan Duhamel <duhadm19@wfu.edu>
2023-08-02 22:05:36 -0400

    Working on hamilton county

--- main.py
+++ main.py
@@ -33,41 +33,41 @@ if __name__ == "__main__":
     Each script will add all available lead info to database
     """
 
-    # cinci_code_enf = CinciCodeEnf()
-    # cinci_code_enf.download_dataset()
-    # cinci_code_enf.start(1)
+    cinci_code_enf = CinciCodeEnf()
+    cinci_code_enf.download_dataset()
+    cinci_code_enf.start(1)
 
-    # cinci_code_enf_API = CinciCodeEnfAPI()
-    # cinci_code_enf_API.start(1)
+    cinci_code_enf_API = CinciCodeEnfAPI()
+    cinci_code_enf_API.start(1)
 
-    # lee_county_code_enf = LeeCountyCodeEnf()
-    # lee_county_code_enf.download_dataset(1)
-    # lee_county_code_enf.start()
+    lee_county_code_enf = LeeCountyCodeEnf()
+    lee_county_code_enf.download_dataset(1)
+    lee_county_code_enf.start()
 
-    # orange_county_code_enf = OrangeCountyCodeEnf()
-    # orange_county_code_enf.download_dataset()
-    # orange_county_code_enf.start(1)
+    orange_county_code_enf = OrangeCountyCodeEnf()
+    orange_county_code_enf.download_dataset()
+    orange_county_code_enf.start(1)
 
-    # # Get today's date and add one day to get tomorrow's date
-    # start_date = datetime.datetime.now() + datetime.timedelta(days=1)
-    # end_date = parse('09/01/2023')
-    # # Generate a list of all dates from start_date to end_date
-    # dates = list(rrule(DAILY, dtstart=start_date, until=end_date))
+    # Get today's date and add one day to get tomorrow's date
+    start_date = datetime.datetime.now() + datetime.timedelta(days=1)
+    end_date = parse('09/01/2023')
+    # Generate a list of all dates from start_date to end_date
+    dates = list(rrule(DAILY, dtstart=start_date, until=end_date))
 
-    # # Iterate over the dates
-    # for date in dates:
-    #     clermont_county_forclosure = ClermontForeclosure(date.strftime("%m/%d/%Y"))
-    #     clermont_county_forclosure.start()
+    # Iterate over the dates
+    for date in dates:
+        clermont_county_forclosure = ClermontForeclosure(date.strftime("%m/%d/%Y"))
+        clermont_county_forclosure.start()
 
-    # # End date
-    # end_date = parse('09/15/2023')
-    # # Generate a list of all dates from start_date to end_date
-    # dates = list(rrule(DAILY, dtstart=start_date, until=end_date))
+    # End date
+    end_date = parse('09/15/2023')
+    # Generate a list of all dates from start_date to end_date
+    dates = list(rrule(DAILY, dtstart=start_date, until=end_date))
 
-    # # Iterate over the dates
-    # for date in dates:
-    #     lee_county_forclosure = LeeCountyForeclosure(date.strftime("%m/%d/%Y"))
-    #     lee_county_forclosure.start()
+    # Iterate over the dates
+    for date in dates:
+        lee_county_forclosure = LeeCountyForeclosure(date.strftime("%m/%d/%Y"))
+        lee_county_forclosure.start()
 
     '''
     Remove any duplicate address from today
