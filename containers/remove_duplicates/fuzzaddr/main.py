047ac0806e6b0f76f866ccf4d9251bdd8b154a9b
Dylan Duhamel <duhadm19@wfu.edu>
2024-07-02 23:30:26 -0400

    Database changes

---
+++ containers/remove_duplicates/fuzzaddr/main.py
@@ -0,0 +1,4 @@
+from postal.parser import parse_address
+res = parse_address(address="4518 35th St North, Arlington, Virginia, 22207")
+
+print(res)
---
+++ containers/remove_duplicates/main.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("/home/dylan/Projects/Acuritas/Acuritas-Lead-Generation")
+print(sys.path)
+from utils.lead_database import testing
+
+
+
+testing()
