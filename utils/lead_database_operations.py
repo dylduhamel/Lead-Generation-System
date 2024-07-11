b1cb25bda14b3bcc0a6359a097ab24540876eb4a
Dylan Duhamel <70403658+dylduhamel@users.noreply.github.com>
2024-07-10 21:30:16 -0400

    Update lead_database_operations.py

--- utils/lead_database_operations.py
+++ utils/lead_database_operations.py
@@ -39,7 +39,7 @@ def add_lead_to_database(full_addr, doc_type):
             session.add(lead)
             session.commit()
         except Exception as e:
-            print(f"Not able to add lead to database. An error has occoured: {e}")
+            print(f"Not able to add lead to database. An error has occurred: {e}")
             session.rollback()
         finally:
             session.close()
